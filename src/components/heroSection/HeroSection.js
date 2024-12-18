6defd1439b3b62be524eebdb6c66d1c1fb23c2d1
JPPUERTAORREGO <jppuertaorrego@gmail.com>
2024-12-18 11:46:17 -0500

    hero section call props

--- src/components/heroSection/HeroSection.js
+++ src/components/heroSection/HeroSection.js
@@ -1,16 +1,24 @@
 import Image from "next/image"
-export const HeroSection = (props) => {
-const {img, slogan, sloganDetail, link} = props
 
+export const HeroSection = (props) => { 
+    const {img, slogan, sloganDetail, link} = props
+    const text = link.text;
+    const href = link.href;
+    const isCta = link.isCta;
+
+    console.log(link)
 
   return (
-    <div>
+    <div>      
         <Image
         src={"https:" + img.url}
         alt="Hero Section"
         width={1439}
         height ={570}
         />
+        <h1>{slogan}</h1>
+        <h5>{sloganDetail}</h5>
+        <a href={href}>{text}</a>
     </div>
   )
 }
