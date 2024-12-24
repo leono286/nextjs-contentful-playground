ffff5c7cee8c522b133f308ca4208fea121bd367
JPPUERTAORREGO <jppuertaorrego@gmail.com>
2024-12-24 11:59:02 -0500

    config hamburguer open menu, pending setup aling content when the menu is open

--- src/components/navBar/Navbar.js
+++ src/components/navBar/Navbar.js
@@ -13,36 +13,36 @@ function Navbar(props) {
 
 
   return (
-  <nav className={styles.navbar}>
-
-    <img
-    className={styles.menuButton}
-    src="/icons/menu.png"
-    onClick={toggleMenu}
-    />
-      <div className={styles.logoContent}>
-      <Image
-      src={`https:${logo.url}`}
-      alt="logo impulsa" 
-      width={55.83}
-      height ={53.24}
+  <nav className={`${styles.navbar} ${menuOpen ? styles.menuOpen : ''}`}>
+     <div className={`${menuOpen ? styles.openHamburguer : styles.logoContent}`}>
+      <img className={styles.menuButton}
+      src={`${menuOpen ? "/icons/close.png":"/icons/menu.png"}`}
+      onClick={toggleMenu}
       />
-      <Image
-      src={`https:${logoText.url}`}
-      alt="logo impulsa text" 
-      width={89.71}
-      height ={20.03}
-      />
-      </div>
-      <div className={styles.contentCTAMobile}>
-        <img className={styles.corazon}
-            src="/icons/favorite.png" 
-            alt="Corazon"
+        <div className={styles.logoContent}>
+          <Image
+          src={`https:${logo.url}`}
+          alt="logo impulsa" 
+          width={55.83}
+          height ={53.24}
           />
+          <Image
+          src={`https:${logoText.url}`}
+          alt="logo impulsa text" 
+          width={89.71}
+          height ={20.03}
+          />
+        </div>
+          {menuOpen ? "" : 
+          <div className={styles.contentCTAMobile}>
+            <img className={styles.corazon}
+                src="/icons/favorite.png" 
+                alt="Corazon"
+              />
+            </div>
+        } 
       </div>
-      
-
-
+   
     <div className={`${styles.linksWrapper} ${menuOpen ? styles.menuOpen : ''}`}>
       {links.map((link) => {
         const text = link.fields.text;
@@ -59,7 +59,8 @@ function Navbar(props) {
           </a>
       })}
     </div>
-  </nav>);
+  </nav>
+  );
 }
 
 export default Navbar;
