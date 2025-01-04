cc835334566835e4044d8f92b5e49c0614dde8c9
JPPUERTAORREGO <jppuertaorrego@gmail.com>
2025-01-04 18:21:18 -0500

    mobile ok, least medias queries

--- src/components/navBar/Navbar.js
+++ src/components/navBar/Navbar.js
@@ -63,7 +63,7 @@ function Navbar(props) {
               alt="Corazon" 
             />  
           )}
-          {text}
+         <p>{text}</p>
           {(index === 1 || index === 2) && (
           <img 
           src="/icons/keyboard_arrow_down.png" 
