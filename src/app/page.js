ec05c86041851291e40589562204a22a8e5748e2
JPPUERTAORREGO <jppuertaorrego@gmail.com>
2025-02-09 12:06:56 -0500

    start work model section, contentful ok and connected

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Navbar from "@/components/navBar/Navbar";
 import { createClient } from "contentful";
 import { MenuProvider } from "../contexts/MenuContext"
 import { WhatIsSection } from "@/components/whatIsSection/WhatIsSection";
+import { WorkModel } from "@/components/workModelSection/WorkModel";
 
 const client = createClient({
   space: process.env.SPACE_ID,
@@ -29,24 +30,32 @@ const getWhatIsSection = async () =>{
   return entries;
 }
 
+const getWorkModelSection = async () =>{
+  const entries = await client.getEntries({content_type: "workModelSection"})
+  return entries;
+}
+
 export default async function Home() {
   const resLogo = await getNavbarLogo();
   const resLinks = await getNavbarLinks();
   const resHeroSection= await getHomeHeroSection();
   const resWhatIsSection= await getWhatIsSection();
+  const resWorkModelSection = await getWorkModelSection();
 
   const navBarLinks = resLinks.items[0].fields.linksArray
   const navBarLogo = resLogo.items[1].fields.navBarLogo.fields.file
   const navBarLogoText = resLogo.items[0].fields.navBarLogo.fields.file
   const heroSectionItems = resHeroSection.items[0].fields.contentArray
   const whatIsSectionItems = resWhatIsSection.items[0].fields.whatIsArray
+  const workModelSectionItems = resWorkModelSection.items[0].fields.workModelArray
 
   return (
     <>
       <MenuProvider>
-        <Navbar logo={navBarLogo} logoText={navBarLogoText} links={navBarLinks} /> 
+        {/* <Navbar logo={navBarLogo} logoText={navBarLogoText} links={navBarLinks} /> 
         <HeroSection items={heroSectionItems}/>
-        <WhatIsSection items={whatIsSectionItems}/>S
+        <WhatIsSection items={whatIsSectionItems}/> */}
+        <WorkModel items={workModelSectionItems}/>
       </MenuProvider>
     </>
   );
---
+++ src/components/workModelSection/WorkModel.js
@@ -0,0 +1,10 @@
+"use client";
+import React from 'react'
+
+export const WorkModel = ({items}) => {
+    console.log(items)
+  return (
+    <div>WorkModel</div>
+  )
+}
+
